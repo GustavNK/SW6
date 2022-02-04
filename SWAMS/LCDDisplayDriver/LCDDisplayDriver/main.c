c84d75b05473c343ae21abe2ed18123d0b5483f5
rasmoller <rasmus28.moller@gmail.com>
2022-02-04 10:17:58 +0100

    AMS LCD initial

---
+++ SWAMS/LCDDisplayDriver/LCDDisplayDriver/main.c
@@ -0,0 +1,18 @@
+/*
+ * LCDDisplayDriver.c
+ *
+ * Created: 02-02-2022 14:52:21
+ * Author : Rasmus
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
