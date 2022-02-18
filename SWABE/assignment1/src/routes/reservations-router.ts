909b10094e2a2d43045418cf1b960f7ca9149b8e
rasmoller <rasmus28.moller@gmail.com>
2022-02-18 11:45:10 +0100

    Small change

--- SWABE/assignment1/src/routes/reservations-router.ts
+++ SWABE/assignment1/src/routes/reservations-router.ts
@@ -5,9 +5,9 @@ import { PERMISSIONS, requirePermission } from '../PermissionsExtensions';
 const router = Router();
 
 router.get('', requirePermission(PERMISSIONS.MANAGER, PERMISSIONS.CLERK), Reservations.list);
-router.get('/:uid', Reservations.read);
+router.get('/:uid', Reservations.read); //Only accessible by guests on their own reservations
 router.post('/:uid', Reservations.create);
-router.patch('/:uid', Reservations.update); //Only accessabe
+router.patch('/:uid', Reservations.update); //Only accessible by guests on their own reservations
 router.delete('/:uid', requirePermission(PERMISSIONS.MANAGER, PERMISSIONS.CLERK), Reservations.remove);
 
 export const reservations = router;
